aba1f9122a5f239e10ae8ee15c72f80d1465cd80
Haider Zaman <hzaman@code-freaks.com>
2024-04-26 16:48:04 +0500

    practice on API params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':username/:password')
+  getHello(
+    @Param('username') username: string,
+    @Param('password') password: string,
+  ): string {
+    return this.appService.getHello(username, password);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(username: string, password: string): string {
+    return `Hello ${username}! AND your password is ${password}`;
   }
 }
